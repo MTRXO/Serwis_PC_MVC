03489f34ff019e6b6662a0b7d50e574827f4e83f
Mateusz <matekudr@gmail.com>
2025-06-23 04:20:18 +0200

    Add db context and references

---
+++ SerwisDataAccess/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SerwisModels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisDataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<RequestModel> Request { get; set; }
+
+    }
+}
