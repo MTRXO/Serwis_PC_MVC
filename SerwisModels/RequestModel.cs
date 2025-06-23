024f8040800de9af716a9fd33dfc5fef7c49db93
Mateusz <matekudr@gmail.com>
2025-06-23 04:02:38 +0200

    Create RequestModel

---
+++ SerwisModels/RequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisModels
+{
+    public class RequestModel
+    {
+        [Required]
+        public int RequestID { get; set; }
+        [Required]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "Wprowadz porawny numer telefonu")]
+        public string PhoneNumber { get; set; }  
+        [Required]
+        [MaxLength(500)]
+        public string RequestContent { get; set; }
+    }
+}
