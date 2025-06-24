a33c74ef8a2c6ce48624798ba87c52caed16a0b6
Mateusz <matekudr@gmail.com>
2025-06-24 03:20:31 +0200

    add IDboperations do DI

--- Serwis_PC/Program.cs
+++ Serwis_PC/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using SerwisDataAccess;
+using SerwisDataAccess.Repository;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped(typeof(IDbOperations<>), typeof(DbOperations<>));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
