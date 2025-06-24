125d7a2f0465f2b938a0ac1df1376de79d0d3317
Mateusz <matekudr@gmail.com>
2025-06-24 02:53:46 +0200

    Add IDbOperations and inplementation

---
+++ SerwisDataAccess/Repository/DbOperations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace SerwisDataAccess.Repository
+{
+    public class DbOperations<T> : IDbOperations<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+   
+        public DbOperations(ApplicationDbContext db)
+        {
+            _db = db;        
+        }
+
+        public void AddRequest(T request)
+        {
+           _db.SaveChanges();
+        }
+    }
+}
---
+++ SerwisDataAccess/Repository/IDbOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisDataAccess.Repository
+{
+    public interface IDbOperations<T> where T : class
+    {
+        void AddRequest(T request);
+    }
+}
